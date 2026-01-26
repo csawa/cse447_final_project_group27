4b31081fa89e0d8a0affd456d87d941d1f37f885
ccskumasan <cc.skumasan@gmail.com>
2026-01-26 10:59:55 -0800

    Update team names in submit.sh

--- submit.sh
+++ submit.sh
@@ -6,7 +6,7 @@ rm -rf submit submit.zip
 mkdir -p submit
 
 # submit team.txt
-printf "Victor Zhong,vzhong\nNoah Smith,nasmith\nSanta Claus,sclaus" > submit/team.txt
+printf "Camie Sawa,csawa\nBen Bioren,bbioren\nMutian Gao,mutiang" > submit/team.txt
 
 # train model
 python src/myprogram.py train --work_dir work
